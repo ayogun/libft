73da32ea29b81f7fd703ecb4bbab9d11c8d87b61
A.YOGUN <yogun@student.42heilbronn.de>
2022-05-10 16:18:23 +0200

    typo

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -19,7 +19,6 @@ t_list	*ft_lstnew(void *content)
 	elt = (t_list *)malloc(sizeof(*elt));
 	if (!elt)
 		return (NULL);
-	(*elt).content = content;
 	elt->content = content;
 	elt->next = NULL;
 	return (elt);
